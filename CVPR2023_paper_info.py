bb4694cdf066112c14b3226a8b1136e73eaa68ff
yzfly <yz.liu.me@gmail.com>
2023-06-24 07:26:19 +0000

    first commit

---
+++ CVPR2023_paper_info.py
@@ -0,0 +1,120 @@
+import os
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+import requests
+import re
+
+def title_to_key(title):
+    title = title.replace("°", "deg")
+    key = re.sub('[^0-9a-zA-Z]+', '_', title)
+    key = key.lower()
+    return key
+
+def extract_thecvf_paper_info(html_file = "cvpr2023.html"):
+
+    # Download webpage using wget
+    if not os.path.exists(html_file):
+        os.system('wget -O {} https://openaccess.thecvf.com/CVPR2023?day=all'.format(html_file))
+    # Open downloaded webpage
+    with open(html_file, 'r') as f:
+        contents = f.read()
+
+    soup = BeautifulSoup(contents, 'html.parser')
+
+    papers_info = {}
+
+    for paper in soup.find_all('dt', {'class': 'ptitle'}):
+        paper_info = {}
+        # Initialize default values
+        paper_info['home_page'] = ""
+        paper_info['pdf'] = ""
+        paper_info['supp'] = ""
+        paper_info['arxiv'] = ""
+        paper_info['bibtex'] = ""
+        paper_info['code'] = ""
+
+        paper_title = paper.get_text(strip=True)
+
+        paper_page = paper.find('a').get('href')
+        paper_info['home_page'] = f'https://openaccess.thecvf.com/{paper_page}'
+
+        paper_links = paper.find_next_sibling('dd').find_next_sibling('dd')
+
+        # Extracting pdf, supp, arxiv links
+        paper_links = paper.find_next_sibling('dd').find_next_sibling('dd').find_all('a')
+        #import ipdb; ipdb.set_trace()
+        for link in paper_links:
+            if 'pdf' in link.text:
+                paper_info['pdf'] = 'https://openaccess.thecvf.com' + link['href']
+            elif 'supp' in link.text:
+                paper_info['supp'] = 'https://openaccess.thecvf.com' + link['href']
+            elif 'arXiv' in link.text:
+                paper_info['arxiv'] = link['href']
+
+        # Extracting bibtex
+        bibtex = soup.find('div', {'class': 'bibref'})
+        paper_info['bibtex'] = bibtex.text.strip()
+        paper_info['title'] = paper_title
+
+        paper_key = title_to_key(paper_title)
+        papers_info[paper_key] = paper_info
+
+    return papers_info
+
+# add code url
+def enrich_paper_info(papers_info):
+    url = 'https://cvpr2023.thecvf.com/Conferences/2023/AcceptedPapers'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    papers = soup.find_all('tr')  # Finding all table row tags
+    paper_list = []  # This will store all the paper information
+
+    for paper in papers:
+        title_link = paper.find('a')  # Finding the first 'a' tag inside the table row
+
+        if not title_link or not title_link['href'].startswith('https://github.com'):
+            continue  # Skip this row if there's no <a> tag or if the href doesn't start with 'https://github.com'
+
+        title = link = authors = ''  # Initialize the variables
+
+        title = title_link.text  # Paper title
+        link = title_link['href']  # GitHub link
+
+        authors_div = paper.find('div', {'class': 'indented'})
+        if authors_div:
+            authors = authors_div.text.strip().split('·')[0].strip()  # First author
+
+        # Append the paper information to the list
+        try:
+            paper_key = title_to_key(title)
+            paper_info = papers_info[paper_key]
+            paper_info['title'] = title
+            paper_info['authors'] = authors
+            paper_info['code'] = link
+        except:
+            paper_info = {}
+            paper_info['title'] = title
+            paper_info['authors'] = authors
+            paper_info['code'] = link
+            print(title)
+
+        paper_list.append(paper_info)
+
+    return paper_list
+
+
+def main():
+    thecvf_papers_info = extract_thecvf_paper_info()
+    paper_list = enrich_paper_info(thecvf_papers_info)
+        # Save the paper list to a JSON file
+    with open('papers_info.json', 'w') as f:
+        json.dump(paper_list, f)
+    print('Saved paper information to papers_info.json.')
+
+
+if __name__ == "__main__":
+    # Call the function
+    main()
+    
---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+import json
+import os
+
+github_api = 'https://api.github.com/repos'
+github_token = ''  # Replace with your GitHub token
+
+def enrich_papers_info():
+    # Load the paper list from the JSON file
+    with open('papers_info.json', 'r') as f:
+        paper_list = json.load(f)
+
+    paper_with_code_list = []
+    for paper in paper_list:
+        link = paper['code']  # GitHub link
+        if link == '':
+            continue
+
+        stars = 0  # Initialize star count
+
+        if 'github.com' in link:
+            parts = link.split('/')
+            if len(parts) > 4:  # Ensure the link points to a specific repository
+                username = parts[3]
+                repo_name = parts[4]
+                # Construct the API URL and send a GET request
+                repo_api = f'{github_api}/{username}/{repo_name}'
+                try:
+                    repo_response = requests.get(repo_api, headers={'Authorization': f'token {github_token}'})
+                    repo_info = json.loads(repo_response.text)
+
+                    # Check the number of stars
+                    stars = repo_info.get('stargazers_count', 0)
+                except Exception as e:
+                    print(f'Error while fetching star count for {link}. Error: {e}')
+
+        # Enrich the paper information with stars and PDF link
+        paper['stars'] = stars
+        paper_with_code_list.append(paper)
+
+    # Save the enriched paper list back to the JSON file
+    with open('papers_with_code_and_stars.json', 'w') as f:
+        json.dump(paper_with_code_list, f)
+
+    print('papers_with_code_and_stars.json saved~')
+
+def write_papers_to_readme():
+    # Load the enriched paper list from the JSON file
+    with open('papers_with_code_and_stars.json', 'r') as f:
+        paper_list = json.load(f)
+
+    # Sort the list by stars
+    sorted_paper_list = sorted(paper_list, key=lambda x: x['stars'], reverse=True)
+
+    with open('CVPR2023.md', 'w') as f:
+        # Write the table headers
+        f.write("# Top CVPR2023 Papers with Code \n")
+        f.write("|Title  | Homepage | Code | Code Stars |\n")
+        f.write("| :---: | :---: | :---: | :---: |\n")
+
+        for paper in sorted_paper_list:
+            # Write the row to the file
+            show_stars = "![GitHub Repo stars](https://badgen.net/github/stars/"+paper['code'].replace("https://github.com/", "")+')'
+            f.write(f"| {paper['title']} | [Paper Link]({paper['home_page']}) | [Code Link]({paper['code']}) | {show_stars}|\n")
+
+    print('Converted enriched paper information to CVPR2023.md.')
+
+if not os.path.exists('papers_with_code_and_stars.json'):
+    enrich_papers_info()
+
+write_papers_to_readme()
